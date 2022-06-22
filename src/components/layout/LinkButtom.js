80017f9de010687c36eca261469af1d697bc57bb
ThalliaB <thalliabrum60@gmail.com>
2022-06-22 14:28:08 -0300

    Add class updates

---
+++ src/components/layout/LinkButtom.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+import styles from './LinkButtom.module.css'
+
+function LinkButtom({ to, text }) {
+    return (
+        <Link className={styles.btn} to={to}>{text}</Link>
+    )
+}
+
+export default LinkButtom
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,68 @@
+import { useLocation } from "react-router-dom"
+import { useState, useEffect } from "react"
+
+import Message from "../layout/Message"
+import styles from "./Projects.module.css"
+import Container from '../layout/Container'
+import Loading from '../layout/Loading'
+import LinkButtom from "../layout/LinkButtom"
+import ProjectCard from "../project/ProjectCard"
+
+
+function Projects() {
+
+    const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+
+    const location = useLocation()
+    let message = ''
+    if(location.state) {
+        message = location.state.message
+    }
+
+    useEffect(() => {
+        setTimeout(
+            () => {
+                fetch('http://localhost:5000/projects', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                .then(resp => resp.json()) 
+                .then(data => {
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))        
+        }, 30000)
+    }, [])
+
+    return (
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus projetos</h1>
+                <LinkButtom to="/newproject" text="Criar Projeto" />
+            </div>
+            {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                {projects.length > 0 && 
+                    projects.map((project) => (
+                        <ProjectCard
+                            id={project.name}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                        />
+                    ))}
+                    {!removeLoading && <Loading/>}
+                    {removeLoading && projects.lenght === 0 && (
+                        <p>Não há projetos cadastrados!</p>
+                    )}
+            </Container>
+        </div>
+    )
+}
+
+export default Projects
